fc8e43b1c03ee16e91745ac9d424338d73389d7e
Daniel Glazer <danielglazer123@gmail.com>
2020-09-27 17:22:47 +0300

    build(npm): configure the project dependencies

---
+++ src/handler.ts
@@ -0,0 +1,15 @@
+// export class Handler {
+// 	private verb: string;
+
+// 	constructor(private command: string) {
+// 		this.verb = this.command[0];
+// 	}
+
+// }
+
+// export class GetHandler extends Handler {
+	
+// 	constructor(command: string) {
+// 		super(command);
+// 	}
+// }
